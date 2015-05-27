be6026f538ea5e7b1219a8faaaacd1aa64073abd
Derek Williams <betteryourweb@gmail.com>
2015-05-27 13:40:56 -0500

    Update file for personal needs

--- resources/views/layouts/main/includes/navbar.blade.php
+++ resources/views/layouts/main/includes/navbar.blade.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Laravel</a>
+                <a class="navbar-brand" href="http://betteryourweb.net">Laravel Remixed <small>(BetterYourWeb)</small></a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
